d0caf9ce9008a2158be78ff3e020b8fb7d2da22a
kosihoon <kosihoon1@gmail.com>
2023-02-14 12:05:12 +0900

    first

---
+++ Source/ProjectSplash/ProjectSplash.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectSplash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSplash, "ProjectSplash" );
---
+++ Source/ProjectSplash/ProjectSplashGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSplashGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSPLASH_API AProjectSplashGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
